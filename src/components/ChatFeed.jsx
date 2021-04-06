c0cc9a511708d137207f39f21d6db5aa7a59fa38
Dibyendu-13 <dibyendubar13702@gmail.com>
2021-04-07 00:34:34 +0530

    1 file modified, 3 added

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import ThierMesage from "./ThierMesage";
+
 const ChatFeed = (props) => {
-  console.log(props)
-  return (<div>ChatFeed</div>);
+  const { chats, activeChat, userName, messages } = props;
+  const chat = chats && chats[activeChat];
+  
+  const RenderMessages=()=>
+  {
+     const keys=Object.keys(messages);
+     return keys.map((key,index)=>
+     {
+       const message=messages[key];
+       const lastMessage=index===0?null:keys[index-1];
+       const isMessage=userName==message.sender.userName;
+         
+       return(
+         <div key={`msg${index}`} style={{width:'100%'}}>
+         <div className="message-block">
+         {
+           isMessage?<MyMessage/>:<ThierMesage/>
+         }
+         </div>
+         <div className="read-reciepts" 
+         style={{
+           marginRight:isMessage
+           ?'18px':'0px',
+            marginLeft:isMessage
+           ?'0px':'68px'
+
+           }}></div>
+         </div>
+         )
+     })
+  };
+  RenderMessages();
+  if(!chat)
+   return "Loading...";
+  return 
+  (<div className="chat-feed" >
+  <div className="chat-title-container">
+  <div className="chat-title">{chat?.title}
+  <
+  </div>
+  </div>
+  </div>);
 };
 
 export default ChatFeed;
---
+++ src/components/TheirMessage.jsx
@@ -0,0 +1,6 @@
+import { React } from "react";
+const ThierMesage = () => {
+  return <div>ThierMesage</div>;
+};
+
+export default ThierMesage;
