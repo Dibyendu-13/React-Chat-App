1dc2e7593597d8779bff7dbbce9467556f23aa57
Dibyendu-13 <dibyendubar13702@gmail.com>
2021-04-07 00:46:24 +0530

    1 file modified

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -41,8 +41,12 @@ const ChatFeed = (props) => {
   return 
   (<div className="chat-feed" >
   <div className="chat-title-container">
-  <div className="chat-title">{chat?.title}
-  <
+  <div className="chat-title">{chat?.title}<div/>
+  <div className="chat-subtitle">
+  {
+    chat.people.map((person)=>`${person.person.userName}`)
+  }
+  </div>
   </div>
   </div>
   </div>);
